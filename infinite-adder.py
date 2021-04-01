a6699f438740bc138eff251f96a3830c1cbca5d6
quailcoral <64557571+quailcoral@users.noreply.github.com>
2021-04-01 16:06:10 -0400

    Add command line argument support

--- infinite-adder.py
+++ infinite-adder.py
@@ -1,19 +1,29 @@
 # Infinite Adder
 # For calculating infinite series
+import argparse
 from fractions import Fraction as frac
 
-# Initial fraction to be added to powers of itself
-fraction = frac(1, 2)
-# Entire series is multiplied by `coefficient`
-coefficient = frac(1, 2)
-# How far into the series to add
-iterations = 10
+# Handle command line arguments
+parser = argparse.ArgumentParser()
 
-# Loop handling variables
+parser.add_argument('fraction', nargs='?', default='1/2', 
+        help='Fraction to add powers of. Default is 1/2')
+parser.add_argument('coefficient', nargs='?', default='1', 
+        help='Coefficient to multiply entire series by. Default is 1')
+parser.add_argument('-i', '--iterations', type=int, default=10, 
+        help='Set number of iterations. Default is 10')
+
+args = parser.parse_args()
+
+# Convert inputs from string to fraction
+fraction = frac(args.fraction)
+coefficient = frac(args.coefficient)
+
+# Variables for looping
 exponent = fraction
 total = coefficient*fraction
 
-for i in range(0, iterations):
+for i in range(0, args.iterations):
     print(str(total)+'='+str(float(total)))
     exponent *= fraction
     total += coefficient*exponent
