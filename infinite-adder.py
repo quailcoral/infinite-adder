99e9ed86d3186967f5ca07181a75b02c5584e61d
quailcoral <64557571+quailcoral@users.noreply.github.com>
2021-04-01 19:37:04 -0400

    Update --help table entries

--- infinite-adder.py
+++ infinite-adder.py
@@ -7,9 +7,9 @@ from fractions import Fraction as frac
 parser = argparse.ArgumentParser()
 
 parser.add_argument('fraction', nargs='?', default='1/2', 
-        help='fraction to add powers of. default is 1/2')
+        help='fraction to define series. default is 1/2')
 parser.add_argument('coefficient', nargs='?', default='1', 
-        help='coefficient to multiply entire series by. default is 1')
+        help='coefficient to multiply with entire series. default is 1')
 parser.add_argument('-i', '--iterations', type=int, default=10, 
         help='set the number of iterations. default is 10')
 
