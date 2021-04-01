5f45165d9a96b96ba1ab0ed0d745d98517241c06
quailcoral <64557571+quailcoral@users.noreply.github.com>
2021-04-01 13:35:54 -0400

    Added Fraction module to handle both fraction and decimal sums.

--- infinite-adding.py
+++ infinite-adding.py
@@ -1,13 +1,19 @@
 # Infinite Adding
 # For exploring infinite series
+from fractions import Fraction as frac
 
-coefficient = 1
-fraction = 0.5
-iteration = 10
-total = fraction
+# Initial fraction to be added to powers of itself
+fraction = frac(1, 2)
+# Entire series is multiplied by `coefficient`
+coefficient = frac(1, 2)
+# How far into the series to add
+iterations = 10
+
+# Loop handling variables
 exponent = fraction
+total = coefficient*fraction
 
-for i in range(0, iteration):
-    print(total)
+for i in range(0, iterations):
+    print(str(total)+'='+str(float(total)))
     exponent *= fraction
-    total += exponent
+    total += coefficient*exponent
